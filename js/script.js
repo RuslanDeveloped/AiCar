e78de9d4ead9d3229893f0fcf9bee63860b15fb4
RuslanDeveloped <110823883+RuslanDeveloped@users.noreply.github.com>
2022-11-10 14:40:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ $('.slider').slick({
   slidesToShow: 1,
   adaptiveHeight: true
 });
+
+$(document).ready(function() {
+
+  $('#contact').on('click', function() {
+
+    //this scroll withour animations in chrome
+    $('.contacts').get(0).scrollIntoView({
+      block: "start",
+      behavior: "smooth"
+    });
+
+  });
+
+  //this scroll WITH animations in all browsers
+
+  //taken from: http://praveenlobo.com/techblog/how-to-scroll-elements-smoothly-in-javascript-jquery-without-plugins/
+
+})
